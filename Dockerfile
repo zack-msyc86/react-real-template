47ce00bdfb3fb24b359efb8b8c8727bbe8c2cee2
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2022-04-02 13:21:18 +0900

    ホットリロードに対応

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM node:alpine
 WORKDIR /app
 COPY package.json yarn.lock ./
 RUN yarn install
-CMD ["node", "build.ts"]
+CMD ["yarn", "serve"]
--- build.ts
+++ build.ts
@@ -1,29 +1,56 @@
-const { build } = require("esbuild");
+const { build } = require('esbuild');
+const bs = require('browser-sync').create();
+const fse = require('fs-extra');
 
-const isDev = process.env.NODE_ENV === '"development"';
-const watch = process.env.WATCH === 'true' || false;
+const isDev = process.argv.includes('--dev');
+const outdir = 'dist';
+
+try {
+  fse.rmSync(outdir, { recursive: true, force: true });
+  fse.mkdirSync(outdir);
+  fse.copyFileSync('src/public/index.html', `${outdir}/index.html`);
+} catch (err) {
+  console.error(`initialize dist failed:`, err);
+  process.exit(1);
+}
 
 build({
-  define: { "process.env.NODE_ENV": process.env.NODE_ENV },
-  target: "es2015",
-  platform: "browser",
-  entryPoints: ["src/main.tsx"],
-  outfile: "src/public/index.js",
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
+  target: 'es2015',
+  platform: 'browser',
+  entryPoints: ['src/main.tsx'],
+  outdir,
   bundle: true,
   minify: !isDev,
   sourcemap: isDev,
   treeShaking: true,
-  watch: watch && {
+  watch: isDev && {
     onRebuild(err, result) {
       console.log(`${new Date().toLocaleString()}: Rebuild`)
       if(err){
-        console.log(JSON.stringify(err?.errors));
+        console.log(err?.errors);
       }
       if(result?.warnings?.length){
-        console.log(JSON.stringify(result?.warnings));
+        console.log(result?.warnings);
       }
     },
   },
-})
-.then(() => console.log(`${new Date().toLocaleString()}: Build Finish`) )
-.catch(err => console.log(`Error: ${JSON.stringify(err)}`));
+}).then(result => {
+  if (isDev) {
+    console.log(`${new Date().toLocaleString()}: Watching source files...`);
+    bs.init({
+      server: outdir,
+      watch: true
+    }, (err) => {
+      if (err) {
+        console.error(`${new Date().toLocaleString()}: Serve Failed:`, err);
+        bs.exit();
+      }
+    });
+  } else {
+    console.log(`${new Date().toLocaleString()}: Build Finish`)
+  }
+}).catch(err => {
+  console.log(`Error: ${JSON.stringify(err)}`)
+  process.exit(1);
+});
