c4bec976ded9846041bb22f872ad60b3f6d1690e
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2022-04-08 19:56:11 +0900

    themeの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
+import { StrictMode, useState } from 'react';
 import Autocomplete from './components/molecules/Autocomplete';
 import Button from './components/atoms/Button';
 import TextField from './components/atoms/TextField';
 
 const App = () => {
-
   const [acValue, setAcValue] = useState('a')
   const acOptions = [
     {value: 'a', label: 'aaa'},
@@ -13,14 +14,16 @@ const App = () => {
     {value: 'd', label: 'ddd'},
   ]
   return (
-    <div>
-      <Button>
-        Hello World
-      </Button>
-      <TextField />
-      <div>{acValue}</div>
-      <Autocomplete<string> options={acOptions} onChange={val => setAcValue(val) } value={acValue}/>
-    </div>
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <Button>
+          Hello World
+        </Button>
+        <TextField />
+        <div>{acValue}</div>
+        <Autocomplete<string> options={acOptions} onChange={val => setAcValue(val) } value={acValue}/>
+      </ThemeProvider>
+    </StrictMode>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+import { green, orange, purple } from "@mui/material/colors";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+})
+
+export default theme
