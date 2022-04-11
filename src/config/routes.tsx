88bac6b64b8f04cddfdbf67cce308a0fe83d5428
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2022-04-11 10:43:54 +0900

    small fix

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,17 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import TopPage from "@/components/pages/TopPage";
-import { ReactNode } from "react";
-type Page = {
-  path: string,
-  element: () => ReactNode
-}
+
 export default () => {
-  const pages: Array<Page> = [
-    { path: '/', element: TopPage }
-  ]
   return (
     <Routes>
-      { pages.map(p => <Route path={p.path} key={p.path} element={p.element()}/>) }
+      <Route index element={<TopPage />} />
     </Routes>
   )
 }
