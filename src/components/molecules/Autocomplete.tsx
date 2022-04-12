cc8ddaa973ae4a0a3e0815f46067e3da29d5d768
zack-msyc86 <masaya.okazaki.zack@gmail.com>
2022-04-12 15:54:43 +0900

    small fix

--- src/components/molecules/Autocomplete.tsx
+++ src/components/molecules/Autocomplete.tsx
@@ -2,19 +2,19 @@ import { Autocomplete as MUIAutocomplete } from '@mui/material'
 import { useMemo, useState } from 'react'
 import TextField from '../atoms/TextField'
 
-type Option = {
-  value: string | null,
+type Option<T> = {
+  value: T | null,
   label: string,
 }
 
-type Props = {
-  options: Array<Option>,
+type Props<T> = {
+  options: Array<Option<T>>,
   label?: string,
   value?: string | null,
-  onChange: (e: string | null) => void,
+  onChange: (e: T | null) => void,
 }
 
-const Autocomplete = ({options, label, onChange, value=null}: Props) => {
+const Autocomplete = <T=string>({options, label, onChange, value=null}: Props<T>) => {
   const [selectedLabel, setSelectedLabel] = useState<string | null>(value)
   const optionLabels = useMemo(() => options.map(o => o.label), [options])
   const handler = (v: string | null) => {
